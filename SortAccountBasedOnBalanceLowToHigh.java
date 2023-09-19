b6e724ee416c309e9f26318cb582b43986f647f1
SudheerMattaparthi <145390225+SudheerMattaparthi@users.noreply.github.com>
2023-09-19 22:09:11 +0530

    Add files via upload

---
+++ SortAccountBasedOnBalanceLowToHigh.java
@@ -0,0 +1,16 @@
+package m1;
+
+import java.util.Comparator;
+
+public class SortAccountBasedOnBalanceLowToHigh implements Comparator<Account>{
+	@Override
+
+	public int compare(Account account1, Account account2) {
+
+		return account1.getBalance()-account2.getBalance();
+
+	}
+
+}
+
+ 
